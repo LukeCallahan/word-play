213d1785ccd7c54ad62b8bf2d9522f42d47ffd3e
LukeCallahan <lukemcal@gmail.com>
2022-10-25 07:54:47 -0700

    add js to complete the prompt

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,25 @@ function getSentence(event) {
   const userInput = document.getElementById('sentences').value;
   console.log(userInput);
   let words = userInput.split(" ");
-  console.log(words);
+  console.log(words[0]);
+  let newArray= [];
+  words.forEach(function(word) {
+    
+    if (word.length >= 3) {
+    newArray.push(word);
+    console.log(newArray);
+    }
+  });
+  newArray.reverse();
+  console.log(newArray);
+  newArray.join();
+  console.log(newArray);
+  let h1 = document.createElement('h1');
+  h1.append(newArray);
+  document.body.append(h1);
+  
 };
 
 window.addEventListener('load', function() {
-  document.querySelector('form').addEventListener('submit',  getSentence());
+  document.querySelector('form').addEventListener('submit',  getSentence);
 })
